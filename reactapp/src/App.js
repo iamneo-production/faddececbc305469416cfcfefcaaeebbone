414126cdeb6aca975876f3ca920d9af4c77b3163
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:56:33 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,7 +5,7 @@ import React,{useState} from 'react';
 
 function App() {
   const[name,setName]=useState('');
-  const[greetings,setGreetings]=useState();
+  const[greeting,setGreeting]=useState();
   const handlechange=(e)=>{
     setName(e.target.value);
   };
@@ -13,13 +13,17 @@ function App() {
     e.preventDefault();
   }
   if(name.trim()!==''){
-    setGreetings('Hello,${name}');
+    setGreeting('Hello,${name}');
   }
   return (
     <div className="App">
       <h2>Hey!!! Greeting</h2>
       <form onSubmit={handlesubmit} data-testing="form">
-        <label data-testid>
+        <label data-testid="label">Enter your name</label>
+        <input type="text" value={name} onChange={handlesubmit}></input>
+        <input type="submit" data-testid="buttonElement">Say Hello</button>
+        <form>
+          {greeting && <h2>{greeting}</h2>}</form>
     </div>
   );
 }
